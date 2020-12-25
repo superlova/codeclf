7da1156c4f1594007b0a57ba9988035b93afe7a3
ForeverKing <49007965+superlova@users.noreply.github.com>
2020-12-25 15:06:12 +0800

    删除__init__中的文本

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,12 +1,12 @@
 '''
 Author: your name
 Date: 2020-12-02 21:43:58
-LastEditTime: 2020-12-20 12:26:01
+LastEditTime: 2020-12-20 12:30:27
 LastEditors: superlova
 Description: In User Settings Edit
 FilePath: /codeclf/preprocessing/__init__.py
 '''
-import sys, os
+# import sys, os
 # project_dir = 'C:/Users/zyt/Documents/GitHub Repositories/codeclf_gui/codeclf'
 # sys.path.append(project_dir)
 # print(os.getcwd())
